5d3eefa981c3287befee1fae9c4abaf133725d3e
Monirul Hasan <kmhasan@gmail.com>
2017-05-30 10:22:43 +0600

    lab task

--- Lab1/src/lab1/Lab1.java
+++ Lab1/src/lab1/Lab1.java
@@ -25,6 +25,25 @@ public class Lab1 {
         Put the minimum of those two variables in a third variable z.
         Print the value of z.
         */
+        
+        /*
+        Task 2:
+        Solve the same problem using Math.min method. Math.min(x, y) should
+        return the minimum value.
+        */
+        
+        /*
+        Task 3:
+        There are many useful methods in the Math class. You can search for
+        "Java Math API" to look at the documentation.
+        https://docs.oracle.com/javase/8/docs/api/java/lang/Math.html
+        Look up the documentation to see how you can compute,
+        a) exponentiation
+        b) square root
+        Calculate and print the value of 2^10 and square root of 450
+        Also print the value of x where
+        int x = 2 ^ 10;
+        */
     }
     
 }
