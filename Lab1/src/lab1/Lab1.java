43404de311e867ad79fdd49e03264ed8bb69b153
Monirul Hasan <kmhasan@gmail.com>
2017-05-30 10:09:44 +0600

    lab task

---
+++ Lab1/src/lab1/Lab1.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab1;
+
+/**
+ *
+ * @author kmhasan
+ */
+public class Lab1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.printf("I am %d years old.\n", 38);
+        
+        int x = 10;
+        /*
+        Task 1:
+        Declare two integer variables, x and y.
+        Put the minimum of those two variables in a third variable z.
+        Print the value of z.
+        */
+    }
+    
+}
