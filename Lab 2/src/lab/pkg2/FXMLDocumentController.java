2aab76889e2f6e58c344d274dc6977c17231d894
Monirul Hasan <kmhasan@gmail.com>
2017-06-06 10:53:01 +0600

    Unit converter

---
+++ Lab 2/src/lab/pkg2/FXMLDocumentController.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab.pkg2;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+/**
+ *
+ * @author kmhasan
+ */
+public class FXMLDocumentController implements Initializable {
+    
+
+    @FXML
+    private TextField inchesField;
+    @FXML
+    private TextField centimetersField;
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void handleLengthConversionAction(ActionEvent event) {
+        String inchesString = inchesField.getText();
+        double inches = Double.parseDouble(inchesString);
+        double centimeters = inches * 2.54;
+        centimetersField.setText("" + centimeters);
+    }
+    
+}
