0695129951ea01220afe19705877b887128989c5
Monirul Hasan <kmhasan@gmail.com>
2017-06-12 12:00:41 +0600

    Calculator

--- Basic Calculator/src/basic/calculator/FXMLDocumentController.java
+++ Basic Calculator/src/basic/calculator/FXMLDocumentController.java
@@ -20,6 +20,10 @@ import javafx.scene.control.TextField;
  */
 public class FXMLDocumentController implements Initializable {
 
+    int num1;
+    int num2;
+    String operation;
+
     @FXML
     private TextField displayField;
     @FXML
@@ -44,6 +48,7 @@ public class FXMLDocumentController implements Initializable {
 
         displayField.setText(newText);
         displayField.requestFocus();
+        displayField.positionCaret(newText.length());
     }
 
     @FXML
@@ -56,4 +61,59 @@ public class FXMLDocumentController implements Initializable {
         displayField.positionCaret(newText.length());
     }
 
+    @FXML
+    private void handleDotAction(ActionEvent event) {
+        String oldText = displayField.getText();
+        String newText = oldText;
+        
+        if (!oldText.contains(".")) {
+            newText = oldText + ".";
+            displayField.setText(newText);
+        }
+        
+        displayField.requestFocus();
+        displayField.positionCaret(newText.length());
+    }
+
+    @FXML
+    private void handleAdditionAction(ActionEvent event) {
+        num1 = Integer.parseInt(displayField.getText());
+        displayField.clear();
+        operation = "ADD";
+    }
+
+    @FXML
+    private void handleSubtractionAction(ActionEvent event) {
+        num1 = Integer.parseInt(displayField.getText());
+        displayField.clear();
+        operation = "SUB";
+    }
+
+    @FXML
+    private void handleEqualsAction(ActionEvent event) {
+        num2 = Integer.parseInt(displayField.getText());
+        int result = 0;
+        if (operation.equals("ADD")) {
+            result = num1 + num2;
+        } else if (operation.equals("SUB")) {
+            result = num1 - num2;
+        }
+        displayField.setText("" + result);
+    }
+    
+    /*
+    Homeworks:
+    1. add all the digits and write code for them
+    2. add operations for * and /
+    3. make sure that you do not have leading zeroes unless
+    that leading zero is followed by a dot. Hint: use 
+    startsWith() method in String class
+        String a = "Hello";
+        a.startsWith("Hell") -> returns true
+        a.startsWith("ell") -> returns false
+        a.contains("ell") -> returns true
+    4. make sure your code handles chaining of operations
+    for example, 10+2+3-4= should give you 11
+
+    */
 }
